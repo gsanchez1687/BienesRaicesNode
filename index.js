530da97c80c3576769cc1d85d550a419f9197127
Guiillermo Enrique <gsanchez1687@gmail.com>
2024-07-03 11:17:35 -0500

    add type module

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log('Hello, world!');
+//crear servidor con express
+import express from 'express';
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/nosotros', (req, res) => {
+    res.send('Nosotros');
+});
+
+app.get('/contacto', (req, res) => {
+    res.json({msj: 'contacto'});
+});
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+    }
+);
+
+
+
